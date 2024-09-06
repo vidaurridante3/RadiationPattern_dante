8f440061f821d06819b4fa0d29d9b063bcf5e791
dante <vidaurridante@tamu.edu.com>
2024-09-06 11:53:29 -0500

    modified:   models/backbone.py 	modified:   train_eval/train_eval_backbone.py

--- train_eval/train_eval_backbone.py
+++ train_eval/train_eval_backbone.py
@@ -4,6 +4,13 @@ from torch.nn import functional as F
 from util.misc import *
 import torch.nn as nn
 from typing import Iterable
+import statistics
+
+
+
+
+#wasn't sure where to put the loss in your arcitucture so i put it here for now 
+
 
 def custom_loss(constant_instance, test_sample):
     # Compute dot product of global feature
@@ -16,8 +23,6 @@ def custom_loss(constant_instance, test_sample):
     loss = -1 * softmax_output[0]
 
     return loss
-
-
 
 #send in augmented golbal features (2N x 1024)
 #send in batchsize N
@@ -44,9 +49,13 @@ def total_loss(batch_size, golbal_features):
 
 
 def train_one_epoch(model: torch.nn.Module, data_loader, optimizer, device, epoch, scaler=None, criterion=None):
+    #set in train mode 
     model.train()
-    
-    loss_total = []
+    criterion.train()
+
+    loss_list = []
+
+    #integrate through inputs calculate the loss
     for input1, input2 in data_loader:
         input1, input2 = input1.to(device), input2.to(device) # decice is cuda or cpu
         
@@ -55,32 +64,38 @@ def train_one_epoch(model: torch.nn.Module, data_loader, optimizer, device, epoc
             
         loss = criterion(output_1, output_2)
         
+        #compute the gradient of loss and update paramaters based on gradients
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
         
-       loss_total.append(loss.item()) 
+        loss_list.append(loss.item()) 
     
-    loss_ = mean(loss_total)
-    print(f"Epoch {epoch}, Loss: {loss_}")
-    return loss_
+    loss_list_avg = statistics.mean(loss_list)
+    print(f"Epoch {epoch}, Loss: {loss_list_avg}")
+    return loss_list_avg
 
 
 def evaluate(model: torch.nn.Module, data_loader, device, criterion):
+    #set to eval 
     model.eval()
-    
-    loss_total = []
+    criterion.eval()
+    loss_list = []
+
+    #integrate through inputs to fill list with losses 
     for input1, input2 in data_loader:
         input1, input2 = input1.to(device), input2.to(device)
         
         with torch.no_grad():
             outputs = model(input1, input2)
             output_1, output_2 = torch.split(outputs, input.size(0), dim=0)
-                
             loss = criterion(output_1, output_2)
-        
-        loss_total.append(loss.item())
-    loss_ = mean(loss_total)
-    print(f"Loss: {loss_}")
-    return loss_
+
+        loss_list.append(loss.item())
+    
+    #take and return avrage loss
+    loss_list_avg = statistics.mean(loss_list)
+    print(f"Loss: {loss_list_avg}")
+    return loss_list_avg
+
         
